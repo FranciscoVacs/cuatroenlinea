707bb7b7782f30aa319e9ea77ea82ddcd6b2f8f2
Francisco Vacs <franciscovacs@gmail.com>
2021-04-19 14:34:36 -0300

    Creacion de una funcion especifica para el limite de columnas

--- prototipo.py
+++ prototipo.py
@@ -8,23 +8,21 @@ def tablero_vacio():
         [0, 0, 0, 0, 0, 0, 0],
     ]
 
+def limite_columna(secuencia):
+    for i in (secuencia):
+        if i < 1 or i > 7:
+            return False
+    return True
+
 def completar_tablero_en_orden(secuencia, tablero):
     j=1
     for n in range(0, len(secuencia)):
-        if secuencia[n] > 0 and secuencia[n] < 8:
-            soltar_ficha_en_columna(j,secuencia[n],tablero)
-            n = n+1
-            if j==1: j = j+1
-            else: j = j-1
-        else:
-            print("\nSecuencia incorrecta")
-            tablero = 0
-            return tablero
+        soltar_ficha_en_columna(j,secuencia[n],tablero)
+        n = n+1
+        if j==1: j = j+1
+        else: j = j-1
     return tablero
             
-
-
-
 def soltar_ficha_en_columna(ficha, columna, tablero):
     for fila in range(6, 0, -1):
         if tablero[fila - 1][columna - 1] == 0:
@@ -32,19 +30,19 @@ def soltar_ficha_en_columna(ficha, columna, tablero):
             return
 
 def dibujar_tablero(tablero):
-    if tablero == 0: print("Intentelo nuevamente")
-    else:
-        print("\nEl tablero es:")
-        for i in tablero:
-            for j in i:
-                print(j, end=" ")
-            print()
-       
+    print("\nEl tablero es:")
+    for i in tablero:
+        for j in i:
+            print(j, end=" ")
+        print()
 
 
 secuencia = [1,2,3,1]
-dibujar_tablero(
-    completar_tablero_en_orden(
-        secuencia, tablero_vacio()
+if (limite_columna(secuencia)):
+    dibujar_tablero(
+        completar_tablero_en_orden(
+            secuencia, tablero_vacio()
+        )
     )
-) 
+else:
+    print("Secuencia Incorrecta") 
